6b9ce8009bedc1bba2d66ac4fad1a1f3745f5c44
Jason Biggs <jasonb@wolfram.com>
2024-08-20 16:40:00 -0500

    bumping paclet version

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,6 +1,6 @@
 Paclet[
     Name -> "WolframClientForPython",
-    Version -> "1.1.9",
+    Version -> "1.1.10",
     MathematicaVersion -> "12.3+",
     Loading -> Automatic,
     Extensions -> {}
